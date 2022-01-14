a419c8a959b9c2b90497971f08f78bc34897f74f
KimDeokHyun <kejrgus0701@gmail.com>
2022-01-14 17:10:11 +0900

    파일썬 1일차 진행

---
+++ Day1/basic_syntax01.py
@@ -0,0 +1,32 @@
+#주석
+print("hello world")
+print('123123')
+
+#숫자연산 출력
+#print() 활용
+print(3+4)
+
+#변수사용
+a = '안녕하세요'
+b = '반갑습니다!!'
+print(a,b,'정말?')
+
+# 수학연산
+
+print(5 + 6)
+print(5 - 6)
+print(5 * 6)
+print(5 / 6)
+print(5 // 3)
+print(2**10)
+
+#정수형 int()
+print(int(5/3))
+
+#실수형 float()
+print(float(5))
+
+#type 입력값 형태 확인 (int, float, str)
+print(type(10))
+print(type(10.0))
+
